b3d9476db0f51f84cf68f4e06c68afec2ff8b40c
ClementMadiot <clementmadiot09@gmail.com>
2024-10-29 12:19:46 +0100

    keypress function & import useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import words from "./data/wordList.json";
 import HangmanDrawing from "./components/HangmanDrawing";
 import HangmanWord from "./components/HangmanWord";
@@ -13,6 +13,29 @@ function App() {
   // console.log(wordToGuess);
   /// All the incorrect letters are the ones where the letter is not in the word to guess
   const incorrectLetters = guessedLetter.filter(letter => !wordToGuess.includes(letter))
+
+  const addGuessedLetter = useCallback((letter: string) => {
+    if (guessedLetter.includes(letter)) return
+
+    setGuessedLetter(currentLetters => [...currentLetters, letter])
+  },[guessedLetter])
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      const key = e.key
+      // if it match between a & z
+      if(!key.match(/^[a-z]$/)) return
+      // otherwise run the function
+      e.preventDefault()
+      addGuessedLetter(key)
+      
+    }
+    document.addEventListener("keypress", handler)
+
+    return () => {
+    document.removeEventListener("keypress", handler)
+    }
+  }, [guessedLetter])
 
   return (
     <div className="max-w-[800px] flex flex-col gap-6 my-0 mx-auto items-center">
