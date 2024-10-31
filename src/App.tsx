05b344d4942850c25a3587f1ea615c4bbaf5bdf3
ClementMadiot <clementmadiot09@gmail.com>
2024-10-31 09:26:58 +0100

    style keyboard active & inactive

--- src/App.tsx
+++ src/App.tsx
@@ -12,30 +12,34 @@ function App() {
   const [guessedLetter, setGuessedLetter] = useState<string[]>([]);
   // console.log(wordToGuess);
   /// All the incorrect letters are the ones where the letter is not in the word to guess
-  const incorrectLetters = guessedLetter.filter(letter => !wordToGuess.includes(letter))
+  const incorrectLetters = guessedLetter.filter(
+    (letter) => !wordToGuess.includes(letter)
+  );
 
-  const addGuessedLetter = useCallback((letter: string) => {
-    if (guessedLetter.includes(letter)) return
+  const addGuessedLetter = useCallback(
+    (letter: string) => {
+      if (guessedLetter.includes(letter)) return;
 
-    setGuessedLetter(currentLetters => [...currentLetters, letter])
-  },[guessedLetter])
+      setGuessedLetter((currentLetters) => [...currentLetters, letter]);
+    },
+    [guessedLetter]
+  );
 
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
-      const key = e.key
+      const key = e.key;
       // if it match between a & z
-      if(!key.match(/^[a-z]$/)) return
+      if (!key.match(/^[a-z]$/)) return;
       // otherwise run the function
-      e.preventDefault()
-      addGuessedLetter(key)
-      
-    }
-    document.addEventListener("keypress", handler)
+      e.preventDefault();
+      addGuessedLetter(key);
+    };
+    document.addEventListener("keypress", handler);
 
     return () => {
-    document.removeEventListener("keypress", handler)
-    }
-  }, [guessedLetter])
+      document.removeEventListener("keypress", handler);
+    };
+  }, [guessedLetter]);
 
   return (
     <div className="max-w-[800px] flex flex-col gap-6 my-0 mx-auto items-center">
@@ -43,7 +47,13 @@ function App() {
       <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
       <HangmanWord guessedLetters={guessedLetter} wordToGuessed={wordToGuess} />
       <div className="self-stretch">
-        <Keyboard />
+        <Keyboard
+          activeLetters={guessedLetter.filter((letter) =>
+            wordToGuess.includes(letter)
+          )}
+          inactiveLetters={incorrectLetters}
+          addGuessedLetter={addGuessedLetter}
+        />
       </div>
     </div>
   );
--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -26,17 +26,27 @@ const KEYS = [
   "y",
   "z",
 ];
-export default function Keyboard() {
-  // const inactive = "opacity-[0.15]"
-  const active = "hover:bg-neutral-300 focus:bg-blue-200 focus:outline-none focus:ring-0 active:bg-neutral-300"
+
+type KeyboardProps = {
+  activeLetters:string[]
+  inactiveLetters:string[]
+  addGuessedLetter: (letter: string) => void
+}
+export default function Keyboard({ activeLetters, inactiveLetters, addGuessedLetter }: KeyboardProps) {
+  const inactive = "opacity-[0.5] bg-red-200"
+  const active = "bg-blue-200"
   return (
     <div className="grid grid-cols-[repeat(auto-fill,minmax(75px,1fr))] gap-2 my-2 ">
       {KEYS.map((key) => {
+        const isActive = activeLetters.includes(key)
+        const isInactive = inactiveLetters.includes(key)
         return (
           <button
             type="button"
-            className={`inline-block rounded-full bg-neutral-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-600 transition-colors focus:outline-none focus:ring-0  ${active} `}
+            className={`inline-block rounded-full bg-neutral-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-neutral-600 transition-colors focus:outline-none focus:ring-0 hover:bg-neutral-300   ${isActive ? active : ''} ${isInactive ? inactive : ''} `}
+            disabled={isInactive || isActive}
             key={key}
+            onClick={() => addGuessedLetter(key)}
           >
             {key}
           </button>
